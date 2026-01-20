42392e7267d3930f24055d37d35a55a8faf1a50d
Saurabh Mishra <sanumishra888@gmail.com>
2026-01-20 17:34:32 +0530

    changes

--- src/app/services/figurine.service.ts
+++ src/app/services/figurine.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, signal, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
