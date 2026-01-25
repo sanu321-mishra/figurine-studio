dd5591d492f5af37ba171fcdb77a79c0d5bab66b
Saurabh Mishra <sanumishra888@gmail.com>
2026-01-25 18:03:15 +0530

    feat: Introduce environment configuration files for development and production, and update gitignore to track them.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    azureFunctionBaseUrl: 'https://figurinstudio-aufxh0eeaua6dwhy.canadacentral-01.azurewebsites.net/api' // Base path
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    azureOpenAIEndpoint: 'YOUR_AZURE_OPENAI_ENDPOINT',
+    deploymentName: 'YOUR_DEPLOYMENT_NAME',
+    azureOpenAIApiKey: 'YOUR_API_KEY'
+};
